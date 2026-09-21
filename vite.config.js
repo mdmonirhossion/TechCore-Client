c561bcc967b739ffdfb87b80a56ec5d6a025f591
Monir Hossion <mdmonirhossion2002@gmail.com>
2026-09-21 15:43:42 +0600

    added to my project all upadaed

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
+        target: 'https://techcore-server.vercel.app',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
